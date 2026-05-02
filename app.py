a61c67c2b6a3360d2e6f396319e6878d0c674025
Hanna Bryant <hannab329@gmail.com>
2026-05-01 23:24:51 -0500

    fixed app folder

--- app.py
+++ app.py
@@ -1,8 +1,70 @@
-from flask import Flask
-from database import init_db
+from flask import Flask, jsonify, request
+from database import ( 
+    init_db,
+      create_driver, get_drivers,
+       create_vehicle, get_vehicles,
+        create_route, get_routes,
+         create_package, get_packages 
+         )
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return jsonify({"message": "Last Mile Logistics Online"})
+
+@app.route("/drivers", methods=["GET"])
+def drivers():
+    return jsonify(get_drivers())
+
+@app.route("/drivers", methods=["POST"])
+def add_driver():
+    data = request.get_json()
+    create_driver(data["name"], data["license_type"])
+    return jsonify({"message":"Driver Created"})
+
+@app.route("/vehicles", methods=["GET"])
+def vehicles():
+    return jsonify(get_vehicles())
+
+@app.route("/vehicles", methods=["POST"])
+def add_vehicle():
+    data = request.get_json()
+    create_vehicle(
+        data["license_plate"], 
+        data["model"],
+        data["driver_id"]
+    )
+    return jsonify({"message": "Vehicle added"})
+
+@app.route("/routes", methods=["GET"])
+def routes():
+    return jsonify(get_routes())
+
+@app.route("/routes", methods=["POST"])
+def add_route():
+    data = request.get_json()
+    create_route(
+        data["date"], 
+        data["service_zone"],
+        data["driver_id"]
+    )
+    return jsonify({"message": "Routed added"})
+
+@app.route("/packages", methods=["GET"])
+def packages():
+    return jsonify(get_packages())
+
+@app.route("/packages", methods=["POST"])
+def add_package():
+    data = request.get_json()
+    create_package(
+        data["description"], 
+        data["weight"],
+        data["route_id"]
+    )
+    return jsonify({"message": "Package added"})
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- database.py
+++ database.py
@@ -84,28 +84,6 @@ def get_drivers():
    conn.close()
    return data
 
-def update_driver(driver_id, name):
-   conn = get_connection()
-   cur = conn.cursor()
-
-   cur.execute(
-      "update driver set name=%s where driver_id=%s",
-      (name, driver_id)
-   )
-   conn.commit()
-   cur.close()
-   conn.close()
-
-def delete_driver(driver_id):
-   conn = get_connection()
-   cur = conn.cursor()
-
-   cur.execute("delete from driver where driver_id=%s", (driver_id,))
-
-   conn.commit()
-   cur.close()
-   conn.close()
-
 
 #VEHICLE============================
 def create_vehicle(license_plate, model, driver_id):
@@ -132,31 +110,6 @@ def get_vehicles():
     cur.close()
     conn.close()
     return data
-
-
-def update_vehicle(vehicle_id, model):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                update vehicle set model=%s where vehicle_id=%s""",
-                (model, vehicle_id))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-def delete_vehicle(vehicle_id):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                delete from vehicle where vehicle_id=%s""", (vehicle_id,))
-
-    conn.commit()
-    cur.close()
-    conn.close()
 
 #ROUTE=================
 
@@ -186,30 +139,6 @@ def get_routes():
     return data
 
 
-def update_route(route_id, service_zone):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                update route set service_zone=%s where route_id=%s""",
-                (service_zone, route_id))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-def delete_route(route_id):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                delete from route where route_id=%s""", (route_id,))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
     #PACKAGE==============
 
 def create_package(description, weight, route_id):
@@ -237,27 +166,3 @@ def get_packages():
     conn.close()
     return data
 
-
-def update_package(package_id, description):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                update package set description=%s where package_id=%s""",
-                (description, package_id))
-
-    conn.commit()
-    cur.close()
-    conn.close()
-
-
-def delete_package(package_id):
-    conn = get_connection()
-    cur = conn.cursor()
-
-    cur.execute("""
-                delete from package where package_id=%s""", (package_id,))
-
-    conn.commit()
-    cur.close()
-    conn.close()
